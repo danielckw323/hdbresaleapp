0286743945c5bdee99a6a83d370bc2793182c110
danielckw323 <danielc323@hotmail.com>
2024-10-27 00:27:46 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -6,28 +6,27 @@ from about_us import about_us
 from hdb_methodology import display_methodology
 
 # Displaying the content
-    st.title("Welcome to the HDB Resale App")
-    st.write("Here’s your app content.")
+st.title("Welcome to the HDB Resale App")
+st.write("Here’s your app content.")
 
-    # Sidebar for navigation
-    st.sidebar.title("HDB Resale App")
-    page = st.sidebar.radio(
-        "Navigation",
-        ("Home", "Resale HDB Simulator", "HDB Policy Explainer", "About Us", "Methodology")
-    )
+# Sidebar for navigation
+st.sidebar.title("HDB Resale App")
+page = st.sidebar.radio(
+    "Navigation",
+    ("Home", "Resale HDB Simulator", "HDB Policy Explainer", "About Us", "Methodology")
+)
 
-    # Page routing
-    if page == "Home":
-        home()
-    elif page == "Resale HDB Simulator":
-        resale_simulator()
-    elif page == "HDB Policy Explainer":
-        policy_explainer()
-    elif page == "About Us":
-        about_us()
-    elif page == "Methodology":
-        display_methodology()
-
+# Page routing
+if page == "Home":
+    home()
+elif page == "Resale HDB Simulator":
+    resale_simulator()
+elif page == "HDB Policy Explainer":
+    policy_explainer()
+elif page == "About Us":
+    about_us()
+elif page == "Methodology":
+    display_methodology()
 
 
 
