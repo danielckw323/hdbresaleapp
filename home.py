30c73802f28819fcb007f6e6190a2a82d94c2e22
danielckw323 <danielc323@hotmail.com>
2024-10-27 19:55:50 +0800

    Add files via upload

--- home.py
+++ home.py
@@ -29,6 +29,13 @@ def home():
     except Exception as e:
         st.error(f"An error occurred: {e}")
 
+st.write("""
+    IMPORTANT NOTICE: This web application is a prototype developed for educational purposes only. The information provided here is NOT intended for real-world usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters. 
+    
+    Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.
+
+    Always consult with qualified professionals for accurate and personalized advice.
+    """)
 
 # Calling the function to render the home page in the app
 home()
